11cd850024916734067f9f700a20342a911d7490
Joey Leger <legerinvestments@gmail.com>
2021-03-11 21:49:18 -0500

    Epic2 (#46)
    * Release v1.1.1
    
    * feat: added figma file
    
    * Bump lodash from 4.17.15 to 4.17.19
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump elliptic from 6.5.2 to 6.5.3
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.2 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.2...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump http-proxy from 1.18.0 to 1.18.1
    
    Bumps [http-proxy](https://github.com/http-party/node-http-proxy) from 1.18.0 to 1.18.1.
    - [Release notes](https://github.com/http-party/node-http-proxy/releases)
    - [Changelog](https://github.com/http-party/node-http-proxy/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/http-party/node-http-proxy/compare/1.18.0...1.18.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix: updated linter rule for comma dangle
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump ini from 1.3.5 to 1.3.7 (#36)
    
    Bumps [ini](https://github.com/isaacs/ini) from 1.3.5 to 1.3.7.
    - [Release notes](https://github.com/isaacs/ini/releases)
    - [Commits](https://github.com/isaacs/ini/compare/v1.3.5...v1.3.7)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump axios from 0.18.1 to 0.21.1 (#37)
    
    Bumps [axios](https://github.com/axios/axios) from 0.18.1 to 0.21.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/v0.21.1/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.18.1...v0.21.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump elliptic from 6.5.3 to 6.5.4 (#38)
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.3 to 6.5.4.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.3...v6.5.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat: added nuxt. setup api route
    
    * Feature weather (#41)
    
    * feat: added weather component, title and spinner
    
    * removed console log
    
    Co-authored-by: Joey Leger <joseph.leger@duke-energy.com>
    
    * Places (#42)
    
    * feat: added Places. removed old app
    
    * fix: updated readme
    
    Co-authored-by: Joey Leger <joseph.leger@duke-energy.com>
    
    * fix: removed click handler from pages/index (#43)
    
    Co-authored-by: Joey Leger <joseph.leger@duke-energy.com>
    
    * remove package lock for now
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Joey Leger <joseph.leger@duke-energy.com>

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Muli:400,700',
+      },
+    ],
+  },
+  serverMiddleware: ['~/serverMiddleware/api'],
+};
